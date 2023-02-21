cec5957bc932cb4b250d475ad3d9c195d729b01d
Ata Mohammadi <atasmohammadi@gmail.com>
2023-02-21 11:43:59 +0100

    fix styling

--- src/screens/products.jsx
+++ src/screens/products.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
@@ -59,6 +58,68 @@ export default function Products() {
 
   const filteredProducts = products ? filterProducts(products, filters) : null;
 
+  const header = (
+    <CardMedia
+      component="img"
+      sx={{
+        mt: "-33px",
+        maxWidth: "100%",
+        maxHeight: "40vh",
+        width: "100%",
+        objectFit: "fill",
+      }}
+      src="https://thumbs.dreamstime.com/b/smartphone-headphones-office-accessories-yellow-background-modern-lifestyle-business-flat-lay-banner-copy-space-176172776.jpg"
+      // src="https://thumbs.dreamstime.com/b/smartphone-office-accessories-yellow-background-modern-lifestyle-business-flat-lay-banner-copy-space-176172335.jpg"
+      alt="header"
+    />
+  );
+
+  const middleSection = (
+    <Grid container>
+      {[
+        {
+          avatar: <LocalShippingOutlinedIcon fontSize="large" />,
+          text: "Free shipping up to $50",
+        },
+        {
+          avatar: <CategoryIcon fontSize="large" />,
+          text: "Product well package",
+        },
+        {
+          avatar: <SupportAgentIcon fontSize="large" />,
+          text: "Support",
+        },
+      ].map((content) => (
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            pt: 4,
+          }}
+        >
+          <Avatar
+            sx={{
+              bgcolor: "#F4C7AB",
+              height: "100px",
+              width: "100px",
+            }}
+            item
+          >
+            {content.avatar}
+          </Avatar>
+          <Box item textAlign={"center"} sx={{ pt: 2 }}>
+            {content.text}
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  );
+
   return (
     <main>
       <Box
@@ -69,110 +130,8 @@ export default function Products() {
           pt: 4,
         }}
       >
-        <Box maxWidth="1">
-          <Typography
-            variant="h5"
-            align="center"
-            color="text.secondary"
-            paragraph
-          >
-            <CardMedia
-              component="img"
-              sx={{
-                mt: "-33px",
-                maxWidth: "100%",
-                maxHeight: "40vh",
-                width: "100%",
-                objectFit: "fill",
-              }}
-              src="https://thumbs.dreamstime.com/b/smartphone-headphones-office-accessories-yellow-background-modern-lifestyle-business-flat-lay-banner-copy-space-176172776.jpg"
-              // src="https://thumbs.dreamstime.com/b/smartphone-office-accessories-yellow-background-modern-lifestyle-business-flat-lay-banner-copy-space-176172335.jpg"
-              alt="header"
-            />
-          </Typography>
-
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              sm={4}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                pt: 4,
-              }}
-            >
-              <Avatar
-                sx={{
-                  bgcolor: "#F4C7AB",
-                  height: "100px",
-                  width: "100px",
-                }}
-                item
-              >
-                <LocalShippingOutlinedIcon fontSize="large" />
-              </Avatar>
-              <Box item textAlign={"center"} sx={{ pt: 2 }}>
-                Free shipping up to $50
-              </Box>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={4}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                pt: 4,
-              }}
-            >
-              <Avatar
-                sx={{
-                  bgcolor: "#B2B8A3",
-                  height: "100px",
-                  width: "100px",
-                }}
-                item
-              >
-                <CategoryIcon fontSize="large" />
-              </Avatar>
-
-              <Box item textAlign={"center"} sx={{ pt: 2 }}>
-                Product well package
-              </Box>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={4}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                pt: 4,
-              }}
-            >
-              <Avatar
-                sx={{
-                  bgcolor: "#abcfd6",
-                  height: "100px",
-                  width: "100px",
-                }}
-                item
-              >
-                <SupportAgentIcon fontSize="large" />
-              </Avatar>
-              <Box item textAlign={"center"} sx={{ pt: 2 }}>
-                Support
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
+        {header}
+        {middleSection}
       </Box>
       <Flex>
         <Left>
