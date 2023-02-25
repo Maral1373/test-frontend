29bf569c6cceaab18e5eae10ae0c7ae213c82fe3
Ata Mohammadi <atasmohammadi@gmail.com>
2023-02-25 15:12:04 +0100

    styling

--- src/screens/productDetails.jsx
+++ src/screens/productDetails.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -37,102 +36,103 @@ export default function ProductDetails() {
     }
   };
 
+  if (!product) {
+    return <Loading />;
+  }
   return (
-    <main>
-      <Container sx={{ my: 0 }} maxWidth="xl">
-        {!product ? (
-          <Loading />
-        ) : (
-          <Card>
-            <Grid container width={"100vw"} spacing={2}>
-              <Grid item xs={12} md={3}>
-                <CardMedia
-                  maxWidth="xl"
-                  component="img"
-                  sx={{
-                    px: 3,
-                    py: 3,
-                    "&:hover": {
-                      transition: "transform 0.15s ease-in-out",
-                      transform: "scale3d(1.05, 1.05, 1)",
-                    },
-                  }}
-                  image={product.info.photo}
-                />
-              </Grid>
-              <Grid item xs={12} md={9}>
-                <CardContent
-                  sx={{
-                    flexGrow: 1,
-                  }}
-                >
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {product.info.name}
-                  </Typography>
-                  <Typography>{product.info.camera}</Typography>
-                  <Typography>
-                    {product.info.displaySize} ({product.info.displayResolution}
-                    )
-                  </Typography>
-                  <Typography>{product.info.cpu}</Typography>
-                  <Typography>{product.info.ram} Ram</Typography>
-                  <Typography>{product.info.internalMemory} Storage</Typography>
-                  <Typography>${product.info.price}</Typography>
-                </CardContent>
-                <CardActions>
-                  <IconButton
-                    size="large"
-                    aria-label="add to shopping cart"
-                    sx={{
-                      "&:hover": {
-                        bgcolor: "#FFF5EB",
-                      },
-                    }}
-                    onClick={() => addToCart(product)}
-                  >
-                    <AddShoppingCartIcon
-                      sx={{
-                        cursor: "pointer",
-                        color: "#d1936d",
-                      }}
-                    />
-                  </IconButton>
-                  <IconButton
-                    aria-label="add to favorites"
-                    sx={{
-                      "&:hover": {
-                        bgcolor: "#FFF5EB",
-                      },
-                    }}
-                    onClick={async () => {
-                      try {
-                        await addToFavorite(product);
-                        setText("Item added to your favorites");
-                        setSeverity("success");
-                        setOpen(true);
-                      } catch (e) {
-                        setText(e);
-                        setSeverity("error");
-                        setOpen(true);
-                      }
-                    }}
-                  >
-                    <FavoriteIcon
-                      sx={{
-                        cursor: "pointer",
-                        color: "#ff8a80",
-                        "&:hover": {
-                          borderColor: "purple",
-                        },
-                      }}
-                    />
-                  </IconButton>
-                </CardActions>
-              </Grid>
-            </Grid>
-          </Card>
-        )}
-      </Container>
-    </main>
+    <Container
+      main
+      sx={{
+        minHeight: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid container maxWidth="xl">
+        <Grid item xs={12} md={4}>
+          <CardMedia
+            component="img"
+            sx={{
+              px: 3,
+              py: 3,
+              "&:hover": {
+                transition: "transform 0.15s ease-in-out",
+                transform: "scale3d(1.05, 1.05, 1)",
+              },
+            }}
+            image={product.info.photo}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <CardContent
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            <Typography gutterBottom variant="h5" component="h2">
+              {product.info.name}
+            </Typography>
+            <Typography>{product.info.camera}</Typography>
+            <Typography>
+              {product.info.displaySize} ({product.info.displayResolution})
+            </Typography>
+            <Typography>{product.info.cpu}</Typography>
+            <Typography>{product.info.ram} Ram</Typography>
+            <Typography>{product.info.internalMemory} Storage</Typography>
+            <Typography>${product.info.price}</Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton
+              size="large"
+              aria-label="add to shopping cart"
+              sx={{
+                "&:hover": {
+                  bgcolor: "#FFF5EB",
+                },
+              }}
+              onClick={() => addToCart(product)}
+            >
+              <AddShoppingCartIcon
+                sx={{
+                  cursor: "pointer",
+                  color: "#d1936d",
+                }}
+              />
+            </IconButton>
+            <IconButton
+              aria-label="add to favorites"
+              sx={{
+                "&:hover": {
+                  bgcolor: "#FFF5EB",
+                },
+              }}
+              onClick={async () => {
+                try {
+                  await addToFavorite(product);
+                  setText("Item added to your favorites");
+                  setSeverity("success");
+                  setOpen(true);
+                } catch (e) {
+                  setText(e);
+                  setSeverity("error");
+                  setOpen(true);
+                }
+              }}
+            >
+              <FavoriteIcon
+                sx={{
+                  cursor: "pointer",
+                  color: "#ff8a80",
+                  "&:hover": {
+                    borderColor: "purple",
+                  },
+                }}
+              />
+            </IconButton>
+          </CardActions>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
